40aaef9b042bcfc395344da519f9dcff29405c29
watermelen63 <wumingxi63@gmail.com>
2026-02-07 20:53:59 +0800

    add analusis.py, main.py's main frame, chat_records.json, and user_traits.json

---
+++ app/main.py
@@ -0,0 +1,20 @@
+import discord
+import os
+import asyncio
+import logging
+import ollama
+import json
+import random
+from dotenv import load_dotenv
+
+app = discord.Bot(intents = discord.Intents.all())
+
+load_dotenv()
+DC_HUNTER_TOKEN = os.getenv("DC_HUNTER_TOKEN")
+
+@app.event
+async def on_ready():
+	print(f"{app.user} is now running!")
+
+if __name__ == "__main__":
+    app.run(DC_HUNTER_TOKEN)
